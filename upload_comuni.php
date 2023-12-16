733bb1d66e0c6f5eea7c03db6e2b57e5a002de34
TOSAT0 <micheletosato05@gmail.com>
2023-12-16 23:04:01 +0100

    Change file upload_comuni.php

--- upload_comuni.php
+++ upload_comuni.php
@@ -8,11 +8,12 @@
     while(!feof($fp)){
         $array = fgetcsv($fp);
         if($array != null){
+            $sigla = $array[0];
             $nome = $array[1];
             $lat = $array[2];
             $lon = $array[3];
         }
-        $conn->query("INSERT INTO comuni VALUES($i,\"$nome\",$lat,$lon)");
+        $conn->query("INSERT INTO comuni VALUES($i,\"$sigla\",\"$nome\",$lat,$lon)");
         $i++;
     }
 
