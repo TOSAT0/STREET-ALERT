0a411599f237be49d9e14e61d71e919c44873682
TOSAT0 <micheletosato05@gmail.com>
2023-12-20 18:09:05 +0100

    Update server.php

--- test/server.php
+++ test/server.php
@@ -1,7 +1,9 @@
 <?php
+    include "func.php";
 
-    $coords = array('lat' => $_GET['lat'], 'lon' => $_GET['lon']);
-
-    echo json_encode($coords);
-
+    if(alert_exist($_GET['lat'], $_GET['lon'])){
+        report("no nuovo", $_GET['lat'], $_GET['lon'], "", 0, 0);
+    }else{
+        report("nuovo", $_GET['lat'], $_GET['lon'], "", 0, 0);
+    }
 ?>
