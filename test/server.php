755c19c5b0dd43b81dda7e5074d675b93b7d5310
TOSAT0 <micheletosato05@gmail.com>
2023-12-28 10:24:53 +0100

    Creation test branch

--- test/server.php
+++ test/server.php
@@ -1,15 +1,33 @@
 <?php
     include "func.php";
 
-    $id_alert = alert_exist($_GET['lat'], $_GET['lon']);
-
-    if($id_alert == -1){
-        report("nuovo", $_GET['lat'], $_GET['lon'], "", 0, 0);
+    if($_GET['status'] == "report"){
+        $id_alert = alert_exist($_GET['lat'], $_GET['lon']);
         
-    }else{
+        if($id_alert == -1){
+            report("photo", $_GET['lat'], $_GET['lon'], "", 0, 0);
+            $response = array("status" => "ok");
+        }else{
+            $response = array("status" => "not_ok");
+        }
+    }
+    if($_GET['status'] == "exist"){
+        $id_alert = alert_exist($_GET['lat'], $_GET['lon']);
         modify($id_alert);
+        $response = array("status" => "ok");
+    }
+    if($_GET['status'] == "not_exist"){
+        report("photo", $_GET['lat'], $_GET['lon'], "", 0, 0);
+        $response = array("status" => "ok");
     }
     
+    header('Content-Type: application/json');
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    echo json_encode($response);
 
+    /* REQUEST STATUS:
+        report      -> check if the alert already exist
+        exist       -> modify the alert in the table
+        not_exist   -> add the alert in the table
+    */
 ?>
