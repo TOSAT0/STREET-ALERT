eb22054e5a0df1655cec1daed51054b78ecbfa66
damianogobbo <cagnin.st.andrea@maxplanck.edu.it>
2024-01-31 14:51:13 +0100

    deleted reverse geocoding

--- Interface V2/src/js/index.js
+++ Interface V2/src/js/index.js
@@ -1,10 +1,7 @@
 import { placeMarkers } from "./segnalazioni.js";
 import { createMap } from "./map.js";
-import { onMapClick } from "./reverse_geocoding.js";
 
 export const map = createMap()
 
 placeMarkers(map)
 
-map.on('click', onMapClick);
-onMapClick
